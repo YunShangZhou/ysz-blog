89ade29666ed7abfb658216a5b9fa4e5c4e48564
Chen Kaibang <yunshangzhou@163.com>
2024-02-19 10:23:52 +0800

    chore: 移除conventional-changelog-atom插件引起的空值误判

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,5 @@
 const Configuration = {
   extends: ['@commitlint/config-conventional'],
-  parserPreset: 'conventional-changelog-atom',
   formatter: '@commitlint/format',
   rules: {
     'type-enum': [
