9f8c7cccd7e3b50a93b6eb83a840e0abbfe9f375
chenkaibang <wb.chenkaibang01@mesg.corp.netease.com>
2024-02-23 13:56:52 +0800

    chore: init

--- apps/backend/src/app/app.controller.ts
+++ apps/backend/src/app/app.controller.ts
@@ -2,12 +2,20 @@ import { Controller, Get } from '@nestjs/common';
 
 import { AppService } from './app.service';
 
-@Controller()
+import { ConfigService } from '@nestjs/config'
+
+@Controller('app')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService , private configService: ConfigService) {}
 
   @Get()
   getData() {
     return this.appService.getData();
+  }
+
+  @Get('getConfig')
+  getConfig(){
+    console.log(`-- this.configService.get('http');`,this.configService.get('http'))
+    return this.configService.get('http');
   }
 }
